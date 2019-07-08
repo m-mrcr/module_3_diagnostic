901994a2f39a14f9e4e1fe1c5c22968d9e34f731
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-08 09:14:42 -0600

    Initial commit

---
+++ spec/features/user_search_returns_foods_path_with_search_results_spec.rb
@@ -0,0 +1,20 @@
+# As a user,
+# When I visit "/"
+# And I fill in the search form with "sweet potatoes"
+# (Note: Use the existing search form)
+# And I click "Search"
+# Then I should be on page "/foods"
+# Then I should see a total of the number of items returned by the search. (531 for sweet potatoes)
+# Then I should see a list of ten foods sorted by relevance.
+#
+# And for each of the foods I should see:
+# - The food's NDB Number
+# - The food's name
+# - The food group to which the food belongs
+# - The food's data source
+# - The food's manufacturer
+
+require 'rails helper'
+
+feature 'user can search for foods'
+  scenario 'by'
